4bfd29259ea12ed9ccb50d4509bc2a3ec389666e
三宅柚希 <yukimiyake38@gmail.com>
2026-03-11 19:55:44 +0900

    feat: app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_project_2026_ver01/src/app.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MyApp();
   }
 }
---
+++ lib/src/features/root/root_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RootScreen extends StatelessWidget {
+  const RootScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Root Screen'),
+      ),
+    );
+  }
+}
