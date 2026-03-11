848d1f296031ea6315902c78b4a8d78a3669759a
三宅柚希 <yukimiyake38@gmail.com>
2026-03-11 20:14:58 +0900

    update: root screen

--- lib/src/features/root/root_screen.dart
+++ lib/src/features/root/root_screen.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:my_project_2026_ver01/src/features/mypage/mypage_screen.dart';
+import 'package:my_project_2026_ver01/src/features/todo_list/todo_list_screen.dart';
 
-class RootScreen extends StatelessWidget {
+class RootScreen extends StatefulWidget {
   const RootScreen({super.key});
 
   @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> {
+  final List<Widget> _pages = [const TodoListScreen(), const MypageScreen()];
+  int _currentIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Root Screen'),
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todo'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Mypage'),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
