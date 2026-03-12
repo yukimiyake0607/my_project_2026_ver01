05575a83c2be2c0071d12ce08e1159caf07d2026
三宅柚希 <yukimiyake38@gmail.com>
2026-03-12 21:30:35 +0900

    feat: todo model and floating action button

---
+++ lib/src/features/todo_list/domain/todo.freezed.dart
@@ -0,0 +1,277 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'todo.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+// dart format off
+T _$identity<T>(T value) => value;
+/// @nodoc
+mixin _$Todo {
+
+ String get id; String get title; bool get isDone;
+/// Create a copy of Todo
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$TodoCopyWith<Todo> get copyWith => _$TodoCopyWithImpl<Todo>(this as Todo, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is Todo&&(identical(other.id, id) || other.id == id)&&(identical(other.title, title) || other.title == title)&&(identical(other.isDone, isDone) || other.isDone == isDone));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,id,title,isDone);
+
+@override
+String toString() {
+  return 'Todo(id: $id, title: $title, isDone: $isDone)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $TodoCopyWith<$Res>  {
+  factory $TodoCopyWith(Todo value, $Res Function(Todo) _then) = _$TodoCopyWithImpl;
+@useResult
+$Res call({
+ String id, String title, bool isDone
+});
+
+
+
+
+}
+/// @nodoc
+class _$TodoCopyWithImpl<$Res>
+    implements $TodoCopyWith<$Res> {
+  _$TodoCopyWithImpl(this._self, this._then);
+
+  final Todo _self;
+  final $Res Function(Todo) _then;
+
+/// Create a copy of Todo
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? id = null,Object? title = null,Object? isDone = null,}) {
+  return _then(_self.copyWith(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as String,title: null == title ? _self.title : title // ignore: cast_nullable_to_non_nullable
+as String,isDone: null == isDone ? _self.isDone : isDone // ignore: cast_nullable_to_non_nullable
+as bool,
+  ));
+}
+
+}
+
+
+/// Adds pattern-matching-related methods to [Todo].
+extension TodoPatterns on Todo {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _Todo value)?  $default,{required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _Todo() when $default != null:
+return $default(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _Todo value)  $default,){
+final _that = this;
+switch (_that) {
+case _Todo():
+return $default(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _Todo value)?  $default,){
+final _that = this;
+switch (_that) {
+case _Todo() when $default != null:
+return $default(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( String id,  String title,  bool isDone)?  $default,{required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _Todo() when $default != null:
+return $default(_that.id,_that.title,_that.isDone);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( String id,  String title,  bool isDone)  $default,) {final _that = this;
+switch (_that) {
+case _Todo():
+return $default(_that.id,_that.title,_that.isDone);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( String id,  String title,  bool isDone)?  $default,) {final _that = this;
+switch (_that) {
+case _Todo() when $default != null:
+return $default(_that.id,_that.title,_that.isDone);case _:
+  return null;
+
+}
+}
+
+}
+
+/// @nodoc
+
+
+class _Todo implements Todo {
+  const _Todo({required this.id, required this.title, required this.isDone});
+  
+
+@override final  String id;
+@override final  String title;
+@override final  bool isDone;
+
+/// Create a copy of Todo
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$TodoCopyWith<_Todo> get copyWith => __$TodoCopyWithImpl<_Todo>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _Todo&&(identical(other.id, id) || other.id == id)&&(identical(other.title, title) || other.title == title)&&(identical(other.isDone, isDone) || other.isDone == isDone));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,id,title,isDone);
+
+@override
+String toString() {
+  return 'Todo(id: $id, title: $title, isDone: $isDone)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$TodoCopyWith<$Res> implements $TodoCopyWith<$Res> {
+  factory _$TodoCopyWith(_Todo value, $Res Function(_Todo) _then) = __$TodoCopyWithImpl;
+@override @useResult
+$Res call({
+ String id, String title, bool isDone
+});
+
+
+
+
+}
+/// @nodoc
+class __$TodoCopyWithImpl<$Res>
+    implements _$TodoCopyWith<$Res> {
+  __$TodoCopyWithImpl(this._self, this._then);
+
+  final _Todo _self;
+  final $Res Function(_Todo) _then;
+
+/// Create a copy of Todo
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? id = null,Object? title = null,Object? isDone = null,}) {
+  return _then(_Todo(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as String,title: null == title ? _self.title : title // ignore: cast_nullable_to_non_nullable
+as String,isDone: null == isDone ? _self.isDone : isDone // ignore: cast_nullable_to_non_nullable
+as bool,
+  ));
+}
+
+
+}
+
+// dart format on
--- lib/src/features/todo_list/todo_list_screen.dart
+++ lib/src/features/todo_list/todo_list_screen.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:my_project_2026_ver01/src/features/todo_list/domain/todo.dart';
 
-class TodoListScreen extends StatelessWidget {
+class TodoListScreen extends StatefulWidget {
   const TodoListScreen({super.key});
+
+  @override
+  State<TodoListScreen> createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  List<Todo> todos = [
+    Todo(id: '1', title: 'Todo 1', isDone: false),
+    Todo(id: '2', title: 'Todo 2', isDone: false),
+    Todo(id: '3', title: 'Todo 3', isDone: false),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,21 +23,28 @@ class TodoListScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: ListView.separated(
           itemBuilder: (context, index) {
+            final todo = todos[index];
             return ListTile(
               leading: Checkbox(
-                value: false,
+                value: todo.isDone,
                 onChanged: (value) {
                   return;
                 },
               ),
-              title: Text('Todo ${index + 1}'),
+              title: Text(todo.title),
             );
           },
           separatorBuilder: (context, index) {
             return const Divider();
           },
-          itemCount: 10,
+          itemCount: todos.length,
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          return;
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
