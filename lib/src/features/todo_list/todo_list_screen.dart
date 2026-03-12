1735466ff7bdc1cf7136d6d4f56f00bd4c097bcb
三宅柚希 <yukimiyake38@gmail.com>
2026-03-12 21:23:13 +0900

    feat: todo list ui

--- lib/src/features/todo_list/todo_list_screen.dart
+++ lib/src/features/todo_list/todo_list_screen.dart
@@ -5,6 +5,28 @@ class TodoListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Todo List Screen')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Todo List')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.separated(
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Checkbox(
+                value: false,
+                onChanged: (value) {
+                  return;
+                },
+              ),
+              title: Text('Todo ${index + 1}'),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const Divider();
+          },
+          itemCount: 10,
+        ),
+      ),
+    );
   }
 }
