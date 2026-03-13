2302ea2dae5a2f894fc9f2d8dd49732b5910c1e9
三宅柚希 <yukimiyake38@gmail.com>
2026-03-13 17:05:51 +0900

    feat: add todo function

--- lib/src/features/todo_list/presentation/todo_list_screen.dart
+++ lib/src/features/todo_list/presentation/todo_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_project_2026_ver01/src/features/todo_list/domain/todo.dart';
 import 'package:my_project_2026_ver01/src/features/todo_list/presentation/components/todo_empty_screen.dart';
 import 'package:my_project_2026_ver01/src/features/todo_list/presentation/todo_list_screen_controller.dart';
 
@@ -34,11 +35,51 @@ class TodoListScreen extends ConsumerWidget {
             : const TodoEmptyScreen(),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          return;
+        onPressed: () async {
+          await _onFloatingActionButtonPressed(ref, context);
         },
         child: const Icon(Icons.add),
       ),
     );
   }
+}
+
+Future<void> _onFloatingActionButtonPressed(
+  WidgetRef ref,
+  BuildContext context,
+) async {
+  final todoController = TextEditingController();
+  await showDialog<void>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('新しいTodoを追加'),
+        content: TextField(
+          controller: todoController,
+          decoration: const InputDecoration(hintText: 'Todoを入力してください'),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('キャンセル'),
+          ),
+          TextButton(
+            onPressed: () {
+              final todoTitle = todoController.text;
+              if (todoTitle.isEmpty) return;
+
+              ref
+                  .read(todoListScreenControllerProvider.notifier)
+                  .addTodo(todoTitle);
+              todoController.clear();
+              Navigator.pop(context);
+            },
+            child: const Text('追加'),
+          ),
+        ],
+      );
+    },
+  );
 }
--- lib/src/features/todo_list/presentation/todo_list_screen_controller.dart
+++ lib/src/features/todo_list/presentation/todo_list_screen_controller.dart
@@ -1,5 +1,6 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:my_project_2026_ver01/src/features/todo_list/domain/todo.dart';
+import 'package:uuid/uuid.dart';
 
 part 'todo_list_screen_controller.g.dart';
 
@@ -8,5 +9,10 @@ class TodoListScreenController extends _$TodoListScreenController {
   @override
   List<Todo> build() {
     return [];
+  }
+
+  void addTodo(String todoTitle) {
+    final newTodo = Todo(id: Uuid().v4(), title: todoTitle, isDone: false);
+    state = [...state, newTodo];
   }
 }
